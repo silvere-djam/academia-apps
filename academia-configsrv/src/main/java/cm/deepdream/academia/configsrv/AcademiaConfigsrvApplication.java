6f9c95025190d12223a9a9f08df52bab2cd18284
deepdream-apps <silvere.djam@gmail.com>
2021-09-06 23:36:38 +0200

    First commit academia-configsrv

---
+++ academia-configsrv/src/main/java/cm/deepdream/academia/configsrv/AcademiaConfigsrvApplication.java
@@ -0,0 +1,17 @@
+package cm.deepdream.academia.configsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class AcademiaConfigsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaConfigsrvApplication.class, args);
+	}
+
+}
